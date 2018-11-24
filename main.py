26e9a6902f20073df2bbdfd48eee3d4e5033e7c4
hicham taybi <36454076+hichamtaybi0@users.noreply.github.com>
2018-11-25 00:04:21 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+# import the necessary packages
+import h5py
+import numpy as np
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import KFold, StratifiedKFold
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# create all the machine learning models
+models = []
+models.append(('LR', LogisticRegression(random_state=9)))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier(random_state=9)))
+models.append(('RF', RandomForestClassifier(n_estimators=3, random_state=9)))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(random_state=9)))
+
+# variables to hold the results and names
+results = []
+names = []
+scoring = "accuracy"
+
+# import the feature vector and trained labels
+h5f_data = h5py.File('Extracted_Features/data.h5', 'r')
+h5f_label = h5py.File('Extracted_Features/labels.h5', 'r')
+
+global_features_string = h5f_data['dataset_1']
+global_labels_string = h5f_label['dataset_1']
+
+global_features = np.array(global_features_string)
+global_labels = np.array(global_labels_string)
+
+h5f_data.close()
+h5f_label.close()
+
+
+# split the training and testing data
+(X_train, X_test, y_train, y_test) = train_test_split(np.array(global_features),
+                                                      np.array(global_labels),
+                                                      test_size=0.2,
+                                                      random_state=9)
+
+
+# 10-fold cross validation
+for name, model in models:
+    kfold = KFold(n_splits=100, random_state=7)
+    cv_results = cross_val_score(model, X_train, y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+# boxplot algorithm comparison
+fig = pyplot.figure()
+fig.suptitle('Machine Learning algorithm comparison')
+ax = fig.add_subplot(111)
+pyplot.boxplot(results)
+ax.set_xticklabels(names)
+pyplot.show()
+
